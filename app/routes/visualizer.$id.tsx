d810b84d1ec1d0308b5fcdea2ae986f855928f4e
meshjohn <meshjohn1@gmail.com>
2026-03-01 19:08:21 +0200

    hoast and upload imagee

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useLocation } from "react-router";
 
 const visualizer = () => {
+  const location = useLocation();
+  const { initialImage, name } = location.state || {};
+
   return (
-    <div>visualizer.$id</div>
-  )
-}
+    <section>
+      <h1>{name || "Untitled Project"}</h1>
+      <div className="visualizer">
+        {initialImage && (
+          <div className="image-container">
+            <h2>Source Image</h2>
+            <img src={initialImage} alt="source" />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default visualizer;
